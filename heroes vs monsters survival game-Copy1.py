ed6b4ca354c167f0fc1d9a64b2652fa8acf1556c
Blinkerz001 <102749056+Blinkerz001@users.noreply.github.com>
2023-09-07 15:25:17 +0100

    Update heroes vs monsters survival game-Copy1.py

--- heroes vs monsters survival game-Copy1.py
+++ heroes vs monsters survival game-Copy1.py
@@ -1,80 +1,87 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[1]:
-
-
-import pygame
 import sys
 import time
 import random
 import numpy as np
 
-
-# In[4]:
-
-
-###this function prints words one letter at a time - copied from google
-###https://stackoverflow.com/questions/9246076/how-to-print-one-character-at-a-time-on-one-line
-
-def delay_print(s):
-    for c in s:
-            sys.stdout.write(c)
-            sys.stdout.flush()
-            time.sleep(0.05)
         
     #class of fighters
 class heroes:
-    def __init__(self, name, types, attack_Move, EVs, health='=========='):
+    def __init__(self, name,location, types, attack_Move, EVs, HP):
         ###makes things in class have those variables
         self.name = name
+        self.location = location
         self.types = types #ranged or melee
         self.attack_Move = attack_Move
         self.damage = EVs['DAMAGE']
         self.defense = EVs['DEFENSE']
-        self.hp = 50 #health points left - 50hp is the default
+        self.HP = HP #health
                 
-def battle(self, heroes2):
-    print(f"--You have encountered a {self.type} attacker on your journey!!!--")
-    print(f"\n{self.name}")
-    print("DAMAGE/", self.damage)
-    print("DEFENSE/", self.defense)
-    print("LVL/", 5*(1+np.mean([self.damage, self.defense])))
-    print("\nVS")
- ### below is your character information - used in battle
-    print(f"\n{heroes2.name}")
-    print("DAMAGE/", heroes2.damage)
-    print("DEFENSE/", heroes2.defense)
-    print("LVL/", 5*(1+np.mean([heroes2.damage, heroes2.defense])))
-    time.sleep(2)
-    
-    rangedAttack = [0,self.damage*abs(distance_between_fighters)] ### damage scaled based on distance between hero and enemy
-    meleeAttack = [0,self.damage/(abs(distance_between_fighters))] ### 0 is there as there will be a chance to miss
-    
-### battle script
-    if (self.hp > 0) and (heroes2.hp > 0):
-        print(f"{self.name} -- HP = {self.hp}")
-        print(f"{heroes2.name} -- HP = {heroes2.hp}")
+    def battle(self, enemy):
+        print(f"--You have encountered a {self.types} attacker on your journey!!!--")
+        print(f"\n{self.name}")
+        print(f"Position is {self.location}")
+        print("DAMAGE/", self.damage)
+        print("DEFENSE/", self.defense)
+        print("\nVS")
+        time.sleep(1)
+ ### below is your character information
+        print(f"\n{enemy.name}")
+        print(f"position is {enemy.location}")
+        print("DAMAGE/", enemy.damage)
+        print("DEFENSE/", enemy.defense)
+        time.sleep(1)
 
-    
-
-    
-
-        ### distance_between_fighters -- need to define this function. should be random start but can be reduced or increased by skipping a turn
-        ### abs value of x1 - x2 + abs value of y1 - y2 
-
+        distance = self.location - enemy.location
+        if self.types == 'melee':
+            self.damage = self.damage/(abs(distance))  ### 0 is there as there will be a chance to miss
+        else:
+            self.damage = self.damage*abs(distance) ### damage scaled based on distance between hero and enemy
+        if enemy.types == 'melee':
+            enemy.damage = enemy.damage/(abs(distance))
+        else:
+            enemy.damage = rangedAttack2 = enemy.damage*abs(distance)  
         
-        ### option on moving closer to opponent or further away
-   
 
+       
 
-# In[ ]:
+    
+        
+### battle script
+        
+        if (self.HP > 0) and (enemy.HP > 0):
+            print(f"{self.name} - HP = {self.HP}")
+            print(f"{enemy.name} - HP = {enemy.HP}")
+             
+            print(f"{self.name} choose a move")
+            choice_self =input("attack (1) or heal (2)")
+            if int(choice_self) == 1:
+                print(f"{self.name} attacked using {self.attack_Move} ")
+                enemy.HP = enemy.HP - self.damage
+            else:
+                self.HP = self.HP + self.defense
+            
+            if enemy.HP <= 0:
+                print("enemy was killed \n congratulations")
+                return
+            
+            else:
+                print(f"{enemy.name} choose a move")
+                choice_enemy =input("attack (1) or heal (2)")
+                if int(choice_enemy) == 1:
+                   print(f"{enemy.name} attacked using {enemy.attack_Move}")
+                   self.HP = self.HP - enemy.damage
+                else:
+                    enemy.HP = enemy.HP + self.defense
 
+            if self.HP <= 0:
+                print(" you have died")
+                return
+            
 
-
-
-
-# In[ ]:
+if __name__ == '__main__':
+    #create attacker
+    warrior1 = heroes('Warrior',random.randint(1,100), 'Melee','slash',{'DAMAGE':float(10),'DEFENSE':float(30)}, float(100) )
+    warrior2 = heroes('Warrior_enemy',random.randint(1,100), 'Melee','slash',{'DAMAGE':float(10),'DEFENSE':float(30)}, float(100) )
 
 
 
